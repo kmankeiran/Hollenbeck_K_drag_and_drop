f99ea711837b9615bcd78a84f56504084703182d
Hollenbeck <k_hollenbeck@tss-hr420-dm19.fc.ca>
2019-01-28 13:37:41 -0500

    placing the puzzle pieces in the web

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -1,16 +1,46 @@
 (() => {
 	console.log('fired');
 
-	const theButton = document.querySelector("#buttonHolder img");
-
-	function changeHeadLine() {
-		document.querySelector("h1").textContent = "Hey there from JS!";
-		document.querySelector("p").textContent = "This is the subhead";
-	}
-
 	// to make sure that your javascript is synced in with the web
 	// set up the puzzle pieces and boards
-	theButton.addEventListener("click", changeHeadLine);
+	//  need a reference to each piece that we want to create
+	const thePieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
+
+	// get a reference to the drag side
+	let piecesBoard = document.querySelector(".puzzle-pieces");
+	let puzzleBoard = document.querySelector(".puzzle-board");
+
+	// get a reference to the buttons at the bottom so we can change the puzzle
+	let puzzleSelectors = document.querySelectorAll("#buttonHolder img");
+
+	// functions go in the middle
+	function createPuzzlePieces(pictureIndex) {
+		// Generate images here -> need to make 4 ( top left, right, bottom left, right)
+		//debugger;
+		// loop through the images refs and generate one for each
+		thePieces.forEach((piece, index) => {
+			let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image"
+				src="images/${piece + pictureIndex}.jpg" alt="puzzle piece">`;
+			piecesBoard.innerHTML += newPuzzlePiece;
+		});
+	}
+
+	function resetPuzzlePieces() {
+		// change the curent puzzle, regenerate the pieces
+		//debugger;
+		// clean out the puzzle pieces div
+		piecesBoard.innerHTML = "";
+
+		// generate new pieces
+		createPuzzlePieces(this.dataset.puzzleref);
+	}
+
+	// event handling goes here
+	puzzleSelectors.forEach(button => button.addEventListener("click", resetPuzzlePieces));
+
+
+	//call this function to set up / generate the pieces on load
+	createPuzzlePieces(0);
 	
 	
 })();
