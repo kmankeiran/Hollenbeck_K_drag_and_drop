a31313a54e53a70a3f2f75ba413bfe7ab31cfb70
Hollenbeck <k_hollenbeck@tss-hr420-dm19.fc.ca>
2019-02-04 13:41:46 -0500

    finished drag and drop

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -13,6 +13,9 @@
 	// get a reference to the buttons at the bottom so we can change the puzzle
 	let puzzleSelectors = document.querySelectorAll("#buttonHolder img");
 
+	// get a reference to the drop areas
+	let dropZones = document.querySelectorAll('.drop-zone');
+
 	// functions go in the middle
 	function createPuzzlePieces(pictureIndex) {
 		// Generate images here -> need to make 4 ( top left, right, bottom left, right)
@@ -20,10 +23,50 @@
 		// loop through the images refs and generate one for each
 		thePieces.forEach((piece, index) => {
 			let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image"
-				src="images/${piece + pictureIndex}.jpg" alt="puzzle piece">`;
+				src="images/${piece + pictureIndex}.jpg" alt="puzzle piece" draggable>`;
 			piecesBoard.innerHTML += newPuzzlePiece;
 		});
+
+		initDrag();
 	}
+
+	// drag and drop functionality
+	//  this is a 3-step process
+	//   1. handle the drag event
+	//   2. handle the dragover event
+	//   3. handle the drop event
+	
+	//   dragging sets some data reference (an audio track name, image source, etc)
+	//   dragiver -> just prevent the default behaviour
+	//   on a drop is where the magic happens -> script that behaviour, get the data reference and 
+	//   do what you need to do with it
+	
+	function initDrag() {
+		piecesBoard.querySelectorAll('img').forEach(img => {
+			img.addEventListener("dragstart", function(e) {
+				console.log('dragin...');
+				e.dataTransfer.setData("text/plain", this.id);
+			});
+
+		});
+	}
+
+	// handle the drop
+	
+	dropZones.forEach(zone => {
+		zone.addEventListener("dragover", function(e) {
+			e.preventDefault();
+			console.log('dragged over me!');
+		});
+
+		zone.addEventListener("drop", function(e) {
+			e.preventDefault();
+			console.log('you dropped somethin on me');
+
+			let piece = e.dataTransfer.getData("text/plain");
+			e.target.appendChild(document.querySelector(`#${piece}`));
+		});
+	});
 
 	function resetPuzzlePieces() {
 		// change the curent puzzle, regenerate the pieces
